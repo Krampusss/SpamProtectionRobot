a500396b08c0ba4255311664a257451c59df1171
Krampusss <90748072+Krampusss@users.noreply.github.com>
2021-12-11 09:09:46 +0530

    Update __main__.py

--- spr/__main__.py
+++ spr/__main__.py
@@ -49,14 +49,14 @@ async def help_command(_, message: Message):
     kb = ikb(
         {
             "Help": "bot_commands",
-            "Repo": "https://t.me/InternationalChatting_CrazyWorld",
+            "Repo": "https://t.me/International_Crazy_World",
             "Add Me To Your Group": f"https://t.me/{BOT_USERNAME}?startgroup=new",
-            "Support Chat (for now)": "https://t.me/InternationalChatting_CrazyWorld",
+            "Support Chat (for now)": "https://t.me/International_Crazy_World",
         }
     )
     mention = message.from_user.mention
     await message.reply_photo(
-        "https://hamker.me/logo_3.png",
+        "https://telegra.ph/file/57b6cbda198a15866b485.jpg",
         caption=f"Hi {mention}, I'm SpamProtectionRobot,"
         + " Choose An Option From Below.",
         reply_markup=kb,
